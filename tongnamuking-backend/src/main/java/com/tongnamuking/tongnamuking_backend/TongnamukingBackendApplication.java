159cadb0f8e50ca193a38d184e5bcef15ab1f8d0
Jeong Hyeonmin <jhm991231@gmail.com>
2025-06-30 17:53:33 +0900

    initial commit

---
+++ tongnamuking-backend/src/main/java/com/tongnamuking/tongnamuking_backend/TongnamukingBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tongnamuking.tongnamuking_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TongnamukingBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TongnamukingBackendApplication.class, args);
+	}
+
+}
---
+++ tongnamuking-backend/src/test/java/com/tongnamuking/tongnamuking_backend/TongnamukingBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tongnamuking.tongnamuking_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongnamukingBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
