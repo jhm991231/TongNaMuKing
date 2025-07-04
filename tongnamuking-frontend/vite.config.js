13d3b3ba1989aa1e7ed8807564220049cf9a9490
Jeong Hyeonmin <jhm991231@gmail.com>
2025-07-04 21:09:54 +0900

    다른 브라우저에서도 작동하게끔

--- tongnamuking-frontend/vite.config.js
+++ tongnamuking-frontend/vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    legacy({
+      targets: ['defaults', 'not IE 11'],
+      polyfills: ['es.promise', 'es.array.includes', 'es.string.includes', 'es.symbol', 'es.object.assign']
+    })
+  ],
+  build: {
+    target: ['es2015', 'chrome63', 'firefox67', 'safari11.1']
+  }
 })
